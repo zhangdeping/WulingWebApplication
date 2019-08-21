68f5a619281fd186aa2a894de8446c42c62b7e43
zhangdeping <894945720@qq.com>
2019-08-22 00:55:24 +0800

    动态字段统计基本完成，分页出问题，待解决

---
+++ WulingWebApplication/Controllers/StatisticsController.cs
@@ -0,0 +1,417 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Webdiyer.WebControls.Mvc;
+using WulingWebApplication.Infrastructure;
+using WulingWebApplication.Models;
+using ALinq.Dynamic;
+using System.Data;
+using System.Dynamic;
+
+namespace WulingWebApplication.Controllers
+{
+    [Authorize]
+    public class StatisticsController : Controller
+    {
+        /// <summary>
+        /// 多条件统计，包括分页，采用MvcPager插件
+        /// </summary>
+        /// <param name="id">
+        /// 页码
+        /// </param>
+        /// <returns></returns>
+        public ActionResult MultiStatistics(int id = 1)
+        {
+            using (var db = new WuLinEntities1())
+            {
+                //ViewData["info"] = "当前数据为示例数据。。。";
+                //var model = db.PassengerVehicles.Take(1).OrderByDescending(a => a.时间).ToPagedList(id, 1);
+                //if (Request.IsAjaxRequest())
+                //    return PartialView("_AjaxSearchPost", model);
+                ViewData["user"] = System.Web.HttpContext.Current.User.Identity.Name;
+                return View();
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Province"></param>
+        /// <param name="City"></param>
+        /// <param name="County"></param>
+        /// <param name="id"> 页码</param>
+        /// <param name="CheckValues">统计参数列表 用空格隔开</param>
+        /// <returns></returns>
+
+        [HttpPost]
+        public ActionResult MultiStatistics(string Province, string City, string County, int id=1, string CheckValues ="")
+        {
+            if(CheckValues == "" || CheckValues == null)
+            {
+                if (Request.IsAjaxRequest())
+                    return PartialView("_AjaxSearchPost", null);
+                return View();
+            }
+            string[] statisticsParam = CheckValues.Split(' ');
+            string groupParams = CheckValues.Replace(' ', ',');
+            string errorMessage = "";
+            errorMessage = "";
+            //using (var db = new WuLinEntities1())
+            //var qry = GetOriginData(Province, City, County, out errorMessage);
+            ViewData["user"] = System.Web.HttpContext.Current.User.Identity.Name;
+            string userName = System.Web.HttpContext.Current.User.Identity.Name;
+            if (userName == null || userName == "")
+            {
+                errorMessage = "您没有权限查询该地域的数据！！";
+
+                return null;
+            }
+
+            AppUser user = UserManager.FindByName(userName);
+            List<Address> listAddress = GetUserAddress(user);
+            if (listAddress == null || listAddress.Count == 0)
+            {
+                errorMessage = "您没有权限查询该地域的数据！！";
+
+                return null;
+            }
+
+            if (AddressListProvinceContainAll(listAddress) == false)//地址列表中没有全部省份
+            {
+                bool isInProvince = false;
+                foreach (var item in listAddress)
+                {
+                    if (item.Province.Contains(Province) || Province.Contains(item.Province) || item.Province.Equals(Province))
+                    {
+                        isInProvince = true;
+                        break;
+                    }
+
+                }
+
+                if (isInProvince == false)
+                {
+                    errorMessage = "您没有权限查询该地域的数据！！当前数据为示例数据。。。";
+
+                    return null;
+                }
+                List<Address> provinceAddressList = new List<Address>();//记录含有本函数Province参数值的地址
+                foreach (var item in listAddress)
+                {
+                    if (item.Province.Contains(Province) || Province.Contains(item.Province) || item.Province.Equals(Province))
+                        provinceAddressList.Add(item);
+                }
+
+                if (AddressListCityContainAll(provinceAddressList) == false)//对应省的地址列表中没有全部市
+                {
+                    bool isInCity = false;
+                    foreach (var item in provinceAddressList)
+                    {
+                        if (item.City.Contains(City) || City.Contains(item.City) || item.City.Equals(City))
+                        {
+                            isInCity = true;
+                            break;
+                        }
+
+                    }
+                    if (isInCity == false)
+                    {
+                        errorMessage = "您没有权限查询该地域的数据！！";
+
+                        return null;
+                    }
+
+                    List<Address> CityAddressList = new List<Address>();//记录对应省含有本函数City参数值的地址
+                    foreach (var item in provinceAddressList)
+                    {
+                        if (item.City.Contains(City) || City.Contains(item.City) || item.City.Equals(City))
+                            CityAddressList.Add(item);
+                    }
+                    if (AddressListCountyContainAll(CityAddressList) == false)//地址列表中没有全部区县
+                    {
+                        bool isInCounty = false;
+                        foreach (var item in CityAddressList)
+                        {
+                            if (item.County.Contains(County) || County.Contains(item.County) || item.County.Equals(County))
+                            {
+                                isInCounty = true;
+                                break;
+                            }
+
+                        }
+                        if (isInCounty == false)
+                        {
+                            errorMessage = "您没有权限查询该地域的数据！！";
+
+                            return null;
+                        }
+                    }
+
+                }
+
+            }
+            var esql = @"select " + groupParams + ",sum(p.保有量) as 合计 from  PassengerVehicle as p " ;//"group by " + groupParams;
+            var qry = db.PassengerVehicles.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(Province) && !Province.Contains("全部"))
+            {
+                esql += @"where  (charindex('"+Province+ "',p.省)>0  or charindex(p.省, '" + Province + "')>0)";
+            }
+                qry = qry.Where(x => x.省.Contains(Province) || Province.Contains(x.省));
+            if (!string.IsNullOrWhiteSpace(City) && !City.Contains("全部"))
+            {
+               esql += @" and charindex('" + City + "',p.市)>0";
+            }
+                qry = qry.Where(x => x.市.Contains(City) || City.Contains(x.市));
+            if (!string.IsNullOrWhiteSpace(County) && !County.Contains("全部"))
+            {
+                esql+= @" and charindex('" + County + "',p.县)>0";
+            }
+
+            esql += @"group by " + groupParams;
+
+            var items = db.Database.SqlQuery<MyDynamicType> (esql);
+
+
+           ViewData["info"] = errorMessage;
+
+           var  model = items.ToPagedList(id, 7);
+
+            if (Request.IsAjaxRequest())
+                return PartialView("_AjaxSearchPost", model);
+            ViewData["user"] = System.Web.HttpContext.Current.User.Identity.Name;
+            return View(model);
+            
+        }
+
+
+        private IQueryable<PassengerVehicle> GetOriginData(string Province, string City, string County,out string errorMessage)
+        {
+            errorMessage = "";
+            //using (var db = new WuLinEntities1())
+            {
+                //var model = db.PassengerVehicles.Take(1).OrderByDescending(a => a.时间).ToPagedList(id, 1);//示例数据
+
+                ViewData["user"] = System.Web.HttpContext.Current.User.Identity.Name;
+                string userName = System.Web.HttpContext.Current.User.Identity.Name;
+                if (userName == null || userName == "")
+                {
+                    errorMessage = "您没有权限查询该地域的数据！！";
+                   
+                    return null;
+                }
+
+                AppUser user =  UserManager.FindByName(userName);
+                List<Address> listAddress = GetUserAddress(user);
+                if (listAddress == null || listAddress.Count == 0)
+                {
+                   errorMessage = "您没有权限查询该地域的数据！！";
+                   
+                    return null;
+                }
+
+                if (AddressListProvinceContainAll(listAddress) == false)//地址列表中没有全部省份
+                {
+                    bool isInProvince = false;
+                    foreach (var item in listAddress)
+                    {
+                        if (item.Province.Contains(Province) || Province.Contains(item.Province) || item.Province.Equals(Province))
+                        {
+                            isInProvince = true;
+                            break;
+                        }
+
+                    }
+
+                    if (isInProvince == false)
+                    {
+                        errorMessage = "您没有权限查询该地域的数据！！当前数据为示例数据。。。";
+                       
+                        return null;
+                    }
+                    List<Address> provinceAddressList = new List<Address>();//记录含有本函数Province参数值的地址
+                    foreach (var item in listAddress)
+                    {
+                        if (item.Province.Contains(Province) || Province.Contains(item.Province) || item.Province.Equals(Province))
+                            provinceAddressList.Add(item);
+                    }
+
+                    if (AddressListCityContainAll(provinceAddressList) == false)//对应省的地址列表中没有全部市
+                    {
+                        bool isInCity = false;
+                        foreach (var item in provinceAddressList)
+                        {
+                            if (item.City.Contains(City) || City.Contains(item.City) || item.City.Equals(City))
+                            {
+                                isInCity = true;
+                                break;
+                            }
+
+                        }
+                        if (isInCity == false)
+                        {
+                            errorMessage = "您没有权限查询该地域的数据！！";
+                           
+                            return null;
+                        }
+
+                        List<Address> CityAddressList = new List<Address>();//记录对应省含有本函数City参数值的地址
+                        foreach (var item in provinceAddressList)
+                        {
+                            if (item.City.Contains(City) || City.Contains(item.City) || item.City.Equals(City))
+                                CityAddressList.Add(item);
+                        }
+                        if (AddressListCountyContainAll(CityAddressList) == false)//地址列表中没有全部区县
+                        {
+                            bool isInCounty = false;
+                            foreach (var item in CityAddressList)
+                            {
+                                if (item.County.Contains(County) || County.Contains(item.County) || item.County.Equals(County))
+                                {
+                                    isInCounty = true;
+                                    break;
+                                }
+
+                            }
+                            if (isInCounty == false)
+                            {
+                                errorMessage = "您没有权限查询该地域的数据！！";
+                                
+                                return null;
+                            }
+                        }
+
+                    }
+
+                }
+
+                var qry = db.PassengerVehicles.AsQueryable();
+                if (!string.IsNullOrWhiteSpace(Province) && !Province.Contains("全部"))
+                    qry = qry.Where(x => x.省.Contains(Province) || Province.Contains(x.省));
+                if (!string.IsNullOrWhiteSpace(City) && !City.Contains("全部"))
+                    qry = qry.Where(x => x.市.Contains(City) || City.Contains(x.市));
+                if (!string.IsNullOrWhiteSpace(County) && !County.Contains("全部"))
+                    qry = qry.Where(x => x.县.Contains(County) || County.Contains(x.县));
+                //var model = qry.OrderByDescending(a => a.时间).ToPagedList(id, 7);
+
+
+                return qry;
+            }
+        }
+
+        private List<Address> GetUserAddress(AppUser user)
+        {
+            List<Address> list = new List<Address>();
+            AppIdentityDbContext db = HttpContext.GetOwinContext().Get<AppIdentityDbContext>();
+            var roles = user.Roles;
+            foreach (var role in roles)
+            {
+                var tempList = db.Addresses.Where(x => x.Role.Id == role.RoleId).ToList();
+                if (tempList != null)
+                {
+                    list.AddRange(tempList);
+                }
+                tempList = null;
+
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// 查找地址列表中的省份是否包含“全部”
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private bool AddressListProvinceContainAll(List<Address> list)
+        {
+            bool flag = false;
+            foreach (var item in list)
+            {
+                if (item.Province.Contains("全部") || "全部".Contains(item.Province))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            return flag;
+        }
+
+        /// <summary>
+        /// 查找地址列表中的城市是否包含“全部”
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private bool AddressListCityContainAll(List<Address> list)
+        {
+            bool flag = false;
+            foreach (var item in list)
+            {
+                if (item.City.Contains("全部") || "全部".Contains(item.City))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+
+            return flag;
+        }
+
+        /// <summary>
+        /// 查找地址列表中的区县是否包含“全部”
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private bool AddressListCountyContainAll(List<Address> list)
+        {
+            bool flag = false;
+            foreach (var item in list)
+            {
+                if (item.County.Contains("全部") || "全部".Contains(item.County))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            return flag;
+        }
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+
+        private AppRoleManager RoleManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppRoleManager>();
+            }
+        }
+
+
+        private WuLinEntities1 db = new WuLinEntities1();
+
+        //private SqlConnection connection;
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            //connection.Close();
+            //connection.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WulingWebApplication/Infrastructure/MyDynamicType.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace WulingWebApplication.Infrastructure
+{
+    public class MyDynamicType:DynamicObject
+    {
+        public int 合计 { get; set; }
+        public string 时间 { get; set; }
+        public string 国产_进口 { get; set; }
+        public string 省 { get; set; }
+        public string 市 { get; set; }
+        public string 县 { get; set; }
+        public string 制造商 { get; set; }
+        public string 车辆型号 { get; set; }
+        public string 品牌 { get; set; }
+        public string 车型 { get; set; }
+        public string 排量 { get; set; }
+        public string 变速器 { get; set; }
+        public string 车辆类型 { get; set; }
+        public string 车身型式 { get; set; }
+        public string 燃油类型 { get; set; }
+        public string 使用性质 { get; set; }
+        public string 所有权 { get; set; }
+        public string 抵押标记 { get; set; }
+        public string 性别 { get; set; }
+        public string 年龄 { get; set; }
+        public string 车身颜色 { get; set; }
+        public string 发动机型号 { get; set; }
+        public string 功率 { get; set; }
+        public string 排放标准 { get; set; }
+        public string 轴距 { get; set; }
+        public string 轮胎规格 { get; set; }
+        public string 车外廓长 { get; set; }
+        public string 车外廓宽 { get; set; }
+        public string 车外廓高 { get; set; }
+        public string 准确排量 { get; set; }
+        public string 核定载客 { get; set; }
+        public string 总质量 { get; set; }
+        public string 整备质量 { get; set; }
+        public string 轴数 { get; set; }
+        public string 前轮距 { get; set; }
+        public string 后轮距 { get; set; }
+        public Nullable<int> 保有量 { get; set; }
+
+
+        Dictionary<string, object> Properties = new Dictionary<string, object>();
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            if (!Properties.Keys.Contains(binder.Name))
+            {
+                //在此可以做一些小动作
+                //if (binder.Name == "Col")
+                //　　Properties.Add(binder.Name + (Properties.Count), value.ToString());
+                //else
+                //　　Properties.Add(binder.Name, value.ToString());
+
+                Properties.Add(binder.Name, value.ToString());
+            }
+            return true;
+        }
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return Properties.TryGetValue(binder.Name, out result);
+        }
+
+    }
+}
